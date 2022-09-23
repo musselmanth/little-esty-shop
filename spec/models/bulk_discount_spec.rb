6153eba1799dd20c8d1842018c75da423a9c71b2
Thomas Musselman <tmusselman@gmail.com>
2022-09-23 15:48:52 -0600

    model test coverage to 100% by testing helper

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'relationships' do
     it {should belong_to :merchant}
   end
+
+  describe 'helper methods' do
+    it 'convert discount decimal to percentage' do
+      discount = create(:bulk_discount, discount: 0.15)
+      expect(discount.discount).to eq(0.15)
+      expect(percent_convert(discount.discount)).to eq("15%")
+    end
+  end
 end
