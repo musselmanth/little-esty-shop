e856c9b0e9b95bf92b61caac663b8bf6541c6306
Thomas Musselman <tmusselman@gmail.com>
2022-09-25 22:38:47 -0600

    add welcome page and merchant index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :merchants, only:[] do
+  resources :merchants, only: [:index] do
     scope module: :merchants do
       get '/dashboard', to: 'dashboard#show', as: :dashboard
       resources :items, except: [:destroy]
       resources :invoices, only: [:index, :show]
-      resources :bulk_discounts, only: [:index, :show, :new, :create, :destroy]
+      resources :bulk_discounts, except: [:update]
       resources :invoice_items, only: [:update]
     end
   end
 
+  get '/', to: 'welcome#index'
+
   namespace :admin do
-    get '/', to: 'dashboard#index'
+    get '/', to: 'dashboard#index', as: :dashboard
     resources :items, except: [:destroy]
     resources :merchants, except: [:destroy]
     resources :invoices, only: [:index, :show, :update]
