eba870d6534f0eebe645306c255a6625887e0248
Thomas Musselman <tmusselman@gmail.com>
2022-09-25 21:17:51 -0600

    added validation tests to model

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe BulkDiscount, type: :model do
     it {should belong_to :merchant}
   end
 
+  describe 'validations' do
+    it { should validate_presence_of :threshold }
+    it { should validate_presence_of :discount }
+    it { should validate_numericality_of(:threshold).with_message("must be a positive whole number") }
+    it { should validate_numericality_of(:discount).with_message("must be a number between 1 and 100") }
+  end
+
   describe 'helper methods' do
     it 'convert discount decimal to percentage' do
       discount = create(:bulk_discount, discount: 15)
