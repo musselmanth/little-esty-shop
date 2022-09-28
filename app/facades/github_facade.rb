4c7830d96a9163670ce095d59054c3e270782772
Thomas Musselman <tmusselman@gmail.com>
2022-09-28 12:18:38 -0600

    github service

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -3,7 +3,8 @@ class GithubFacade
   def self.generate_repo
     name = GithubService.repo[:name]
     contributors = GithubService.contributors[0..4]
-    team_members = contributors.map{|contributor| {login: contributor[:login], commits: contributor[:contributions]}}
+    if contributors
+    team_members = contributors.map{|contributor| {login: contributor[:login], commits: contributor[:contributions]}}  if contributors
     pr = GithubService.get_pulls[0][:number]
     Repo.new(name, team_members, pr)
   end
