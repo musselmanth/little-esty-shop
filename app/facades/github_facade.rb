1bc409d094775d2f131c2d284d4cd8d7d3fc9362
Thomas Musselman <tmusselman@gmail.com>
2023-02-09 15:27:58 -0700

    updated github facade to still load with github api returning error

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -1,10 +1,14 @@
 class GithubFacade
 
   def self.generate_repo
-    name = GithubService.repo[:name]
-    contributors = GithubService.contributors[0..4]
-    team_members = contributors.map{|contributor| {login: contributor[:login], commits: contributor[:contributions]}}
-    pr = GithubService.get_pulls[0][:number]
+    if GithubService.repo.has_key?(:id)
+      name = GithubService.repo[:name]
+      contributors = GithubService.contributors[0..4]
+      team_members = contributors.map{|contributor| {login: contributor[:login], commits: contributor[:contributions]}} if contributors
+      pr = GithubService.get_pulls[0][:number]
+    else
+      name, contributors, team_members = nil, nil, []
+    end
     Repo.new(name, team_members, pr)
   end
 end
